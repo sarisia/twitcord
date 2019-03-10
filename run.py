a5c9996de216c9e9b8f238697d057889472714ae
Sophie Neuenmuller <sarisiaice@gmail.com>
2019-03-10 22:54:08 +0900

    fix logger

--- run.py
+++ run.py
@@ -2,7 +2,10 @@ import logging
 
 from twitcord import Twitcord
 
-#logging.basicConfig(level=logging.DEBUG, handlers=[logging.NullHandler()])
+handler = logging.StreamHandler()
+#handler.addFilter(lambda module: module.name.split('.')[0] in ['root', 'twitcord'])
+
+logging.basicConfig(level=logging.DEBUG, handlers=[handler])
 log = logging.getLogger()
 
 # TODO: need launcher to handle signal
